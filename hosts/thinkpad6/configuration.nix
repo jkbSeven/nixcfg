6d8aa8142e56b918d19dcd51bbf6c9c407b4d54e
jkbSeven <Jacob202@protonmail.com>
2026-05-19 12:42:50 +0200

    added docker in rootless mode to thinkpad6 configuration

--- hosts/thinkpad6/configuration.nix
+++ hosts/thinkpad6/configuration.nix
@@ -47,6 +47,14 @@
     };
   };
 
+  virtualisation.docker = {
+      enable = true;
+      rootless = {
+          enable = true;
+          setSocketVariable = true;
+      };
+  };
+
   time.timeZone = "Europe/Warsaw";
 
   i18n.defaultLocale = "en_US.UTF-8";
