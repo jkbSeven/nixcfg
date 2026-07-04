03e20f23d7aea44b4ef3099861a1d0a3feb54f70
jkbSeven <Jacob202@protonmail.com>
2026-07-05 01:27:23 +0200

    set a configuration limit for boot entries for pc

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -7,7 +7,10 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 5;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.binfmt.emulatedSystems = [
